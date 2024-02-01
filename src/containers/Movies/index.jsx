b450e4f5babccdf08d50f3a5ee663a2852734a93
Bruhnodev17 <manoelbrunop17@gmail.com>
2024-02-01 17:54:53 -0300

    Rotas da aplicação

---
+++ src/containers/Movies/index.jsx
@@ -0,0 +1,11 @@
+function Movies(){
+
+    return(
+        <div>
+            <h1>movies</h1>
+            <p>This is Movies</p>
+        </div>
+    )
+}
+
+export default Movies
---
+++ src/containers/Series/index.jsx
@@ -0,0 +1,11 @@
+function Series(){
+
+    return(
+        <div>
+            <h1>Series</h1>
+            <p>This is Series</p>
+        </div>
+    )
+}
+
+export default Series
