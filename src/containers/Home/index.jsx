79b1446b1b6bbd9969a8eb85e2143584ad049cc9
Bruhnodev17 <manoelbrunop17@gmail.com>
2024-02-02 13:34:00 -0300

    Adiconando UseState

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,18 +1,24 @@
 import api from "../../services/api"
 import { Background } from "./styles"
+import { useState } from "react"
 
 function Home() {
 
     async function getMovies() {
+
+        const [movie, setMovie] = useState()
+
         const data = await api.get("/movie/popular")
+        setMovie(data.data.results[1])
+        console.log(movie)
         console.log(data)
     }
     getMovies()
 
     return (
         <Background img="">
-            <h1>Home</h1>
-            <p>This is Home</p>
+            <h1>{movie.title}</h1>
+            <p>{movie.overview}</p>
         </Background>
     )
 }
